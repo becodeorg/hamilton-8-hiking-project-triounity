7ddea81195d2458cdfc1e56367793172d8f3fd9c
Pauline Neuville <pauline.neuville@gmail.com>
2023-08-21 16:11:29 +0200

    modif de fct

--- src/public/Index.php
+++ src/public/Index.php
@@ -5,19 +5,14 @@
 include_once "inc/default.inc.php";
 require_once "vendor/autoload.php";
 
-/*
 
-use core\Router;e
+
+use core\Router;
 
 $router = new Router();
 $router->route(
-    parse_url(ee
+    parse_url(
         $_SERVER['REQUEST_URI'],
         PHP_URL_PATH
     )
 );
-
-
-*/?><!--
-
--->
