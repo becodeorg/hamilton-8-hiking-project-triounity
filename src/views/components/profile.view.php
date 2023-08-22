e60e5974f8f51408a8f47293e9e290d664693306
steeve0304 <zychsteeve4@gmail.com>
2023-08-22 17:00:50 +0200

    profil

--- src/views/components/profile.view.php
+++ src/views/components/profile.view.php
@@ -1,16 +1,8 @@
-<!-- profile.view.php -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profil</title>
-</head>
-<body>
+<div class="profile-container">
     <h1>Profil de <?php echo $userData['nickname']; ?></h1>
     <p>Nom complet : <?php echo $userData['firstname'] . ' ' . $userData['lastname']; ?></p>
     <p>Email : <?php echo $userData['email']; ?></p>
+
     <!-- Ajoutez d'autres informations du profil ici -->
     <a href="/update-profile">Mettre à jour le profil</a>
-</body>
-</html>
+</div>
